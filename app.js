df7f5670be323bb7f210a6132ce0dca14d4cb58c
Pita Badila <ritualmomma@gmail.com>
2023-06-08 01:17:42 -0400

    Adds cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -14,7 +15,7 @@ const authRouter = require('./routes/auth');
 const journalsRouter = require('./routes/journals');
 
 var app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
