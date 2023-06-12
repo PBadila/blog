ea94842370d8182b33af4a3d0b1958d926c2ac01
Pita Badila <ritualmomma@gmail.com>
2023-06-12 11:34:57 -0400

    Adds route to update blog

--- routes/journals.js
+++ routes/journals.js
@@ -38,4 +38,39 @@ router.get('/:id', async (req,res) => {
     }
 })
 
+//Update a Blog Post by id
+router.put('/:id', async (req,res)=> {
+    const { title, entry, link, topic } = req.body
+    try{
+        //newPost is an empty object created to hold the properties to be updated
+        const newPost = {}
+        //checks to see if each of these attributes are being updated, so if they exist, add them to the newly created object holding the properties to be updated
+        if(title !== undefined){
+            newPost.title = title
+        }
+        if(entry !== undefined){
+            newPost.entry = entry
+        }
+        if(link !== undefined){
+            newPost.link = link
+        }
+        if(topic !== undefined){
+            newPost.topic = topic
+        }
+        const [updated] = await Journal.update(newPost, {
+            where: {id:req.params.id},
+        
+        })
+        if (updated) {
+            const updatedPost = await Journal.findByPk(req.params.id)
+            res.json(updatedPost)
+        } else{
+            res.status(404).json({message: 'Post not found'})
+        }
+        } catch (error) {
+            res.status(500).json({message: "Error updating post", error})
+        }
+    
+})
+
 module.exports = router;
